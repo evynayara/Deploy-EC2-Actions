a92b1b53a47df6795ac13d71f01c9a62990c9f54
Evelyn <evenay99@gmail.com>
2024-06-24 17:55:34 -0300

    aula6

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"> </script>
+
+    </head>
+    <body class="antialiased">
+
+
+       
 <h1> Tela de Produtos </h1>
+
+</body>
+
+</head>
